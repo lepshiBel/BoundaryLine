f808fc7e9cfe8b697756ba2fc433209c69aef254
lepshiBel <eliseyavvakumov@mail.ru>
2023-05-20 15:44:42 +0300

    Update AccelerometerObjectMovement.cs
    Updated borders of platform movement.

--- Assets/Scripts/AccelerometerObjectMovement.cs
+++ Assets/Scripts/AccelerometerObjectMovement.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class AccelerometerObjectMovement : MonoBehaviour
 {
+    public GameObject side;
+
+    private Camera camera;
+
+    private float minX, maxX, minY, maxY;
+
     private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
     private float speed = 30.0f;
-    private const float BorderPosition = 3f;
 
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        camera = Camera.main;
+
+        float objectSize = side.GetComponent<Renderer>().bounds.size.x;
+        float cameraDistance = Mathf.Abs(transform.position.z - camera.transform.position.z);
+
+        Vector3 cameraMin = camera.ViewportToWorldPoint(new Vector3(0, 0, cameraDistance));
+        Vector3 cameraMax = camera.ViewportToWorldPoint(new Vector3(1, 1, cameraDistance));
+
+        minX = cameraMin.x + objectSize;
+        maxX = cameraMax.x - objectSize;
+
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -22,7 +36,7 @@ public class AccelerometerObjectMovement : MonoBehaviour
 
         float positionX = rb.position.x + dir.x * speed * Time.deltaTime;
 
-        positionX = Mathf.Clamp(positionX, -BorderPosition + (spriteRenderer.size.x / 2), BorderPosition - (spriteRenderer.size.x / 2));
+        positionX = Mathf.Clamp(positionX, minX, maxX);
 
         rb.MovePosition(new Vector2(positionX, rb.position.y));
     }
